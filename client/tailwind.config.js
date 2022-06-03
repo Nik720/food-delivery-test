d3981154ff6444e3900317794562bd5eaabdf7bc
Nik720 <nikunj.beladiya@tarento.com>
2022-06-04 02:32:32 +0530

    Added tailwind css and default font family

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,5 +3,13 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-  ]
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Nunito', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
 }
