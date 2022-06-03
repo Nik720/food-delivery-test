010e9c19d4c6267bf81d4b74de55469ed3031f42
Nik720 <nikunj.beladiya@tarento.com>
2022-06-04 02:33:02 +0530

    Added Basic Login page

---
+++ client/src/pages/Login-page.js
@@ -0,0 +1,10 @@
+import "../Login/Login-page.css"
+const LoginPage = () => {
+    return (
+        <div className="h-screen flex">
+            Login page
+        </div>
+    );
+}
+
+export default LoginPage;
