27486429f99f4310a419467af3be1bea891be3a7
Nik720 <nikunj.beladiya@tarento.com>
2022-06-05 01:38:12 +0530

    Looking for port in process

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ const envVars = dotenv.config().parsed;
 module.exports = {
     api_host: "http://localhost:5001/api/v1/" ,
     env: process.env.NODE_ENV || envVars.NODE_ENV,
-    port: process.env.PORT || envVars.PORT,
+    port: process.env.SERVER_PORT || envVars.PORT,
     mongoose: {
       url: process.env.MONGO_URI || envVars.MONGO_URI + (envVars.NODE_ENV === 'test' ? '-test' : ''),
       options: {
