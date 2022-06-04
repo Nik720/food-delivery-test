1790bc89b9dda362fd02441c4bdf4939943f7778
Nik720 <nikunj.beladiya@tarento.com>
2022-06-05 00:57:43 +0530

    Added changes to deploy on heroku

--- app.js
+++ app.js
@@ -35,4 +35,12 @@ app.use(compression());
 app.use(cors());
 app.options('*', cors());
 
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 module.exports = app;
