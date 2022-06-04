dd723d1b10ffb8915b4089759b14b988d6a1f695
Nik720 <nikunj.beladiya@tarento.com>
2022-06-05 00:46:17 +0530

    Modified env variables

--- config/config.js
+++ config/config.js
@@ -4,10 +4,10 @@ const dotenv = require('dotenv');
 const envVars = dotenv.config().parsed;
 module.exports = {
     api_host: "http://localhost:5001/api/v1/" ,
-    env: envVars.NODE_ENV,
+    env: process.env.NODE_ENV || envVars.NODE_ENV,
     port: envVars.PORT,
     mongoose: {
-      url: envVars.MONGO_URI + (envVars.NODE_ENV === 'test' ? '-test' : ''),
+      url: process.env.MONGO_URI || envVars.MONGO_URI + (envVars.NODE_ENV === 'test' ? '-test' : ''),
       options: {
         useCreateIndex: true,
         useNewUrlParser: true,
