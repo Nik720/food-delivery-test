b8438d284a227854be66d93ccc65241786eee62a
Nik720 <nikunj.beladiya@tarento.com>
2022-06-08 11:08:04 +0530

    Allowed localhost

--- app.js
+++ app.js
@@ -40,7 +40,12 @@ app.use(mongoSanitize());
 app.use(compression());
 
 // enable cors
-app.use(cors());
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": true,
+  "optionsSuccessStatus": 200
+}));
 app.options('*', cors());
 
 // jwt authentication
